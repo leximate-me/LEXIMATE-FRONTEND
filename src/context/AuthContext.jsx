8109e35fef77ad55d6d9f39ca5f65fd89a82ceaa
GastonFlorenciano <gaston__florenciano@hotmail.com>
2024-10-02 12:22:55 -0300

    SOLO EL USUARIO CON ROL DE PROFESOR PUEDE ACCEDER AL FORMULARIO PARA CREAR UNA CLASE

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -40,7 +40,6 @@ const AuthProvider = ({ children }) => {
   const signIn = async (user) => {
     try {
       const res = await loginRequest(user);
-      console.log('login',res);
       setIsAuthenticated(true);
       setUser(res.data);
       console.log(res.data);
--- src/pages/ClassPage.jsx
+++ src/pages/ClassPage.jsx
@@ -2,14 +2,18 @@ import { useEffect, useState } from 'react';
 import ClassCard from '../components/ClassCard';
 import { useClass } from '../context/ClassContext';
 import { FaPlus } from 'react-icons/fa';
+import { useAuth } from '../context/AuthContext';
 import CreateClassModal from '../components/ClassForm'; // Importar el componente del formulario
 
 function TaskPage() {
   const { getClasses, classes } = useClass();
   const [showModal, setShowModal] = useState(false);  // Controla si el modal está visible o no
 
+  const { user } = useAuth();
+
   useEffect(() => {
     getClasses();
+    console.log('user rol',user.roles_fk);
   }, []);
 
   return (
@@ -19,23 +23,26 @@ function TaskPage() {
       </div>
 
       {/* Botón flotante para crear clase */}
-      <div className="fixed bottom-8 right-8">
-        <button
-          onClick={() => setShowModal(true)}  // Mostrar el modal al hacer clic
-          className="relative w-14 h-14 bg-blue-600 text-white rounded-full p-4 hover:bg-blue-700 transition duration-200 group"
-        >
-          <FaPlus className="absolute left-5 bottom-5" />
-          <span className="absolute bottom-full mb-2 w-20 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs rounded py-1 px-2 opacity-0 transition-opacity duration-200 pointer-events-none group-hover:opacity-100">
-            Crear una clase
-          </span>
-        </button>
-      </div>
+      {user.roles_fk === 3 ? (
+        <>
+          <div className="fixed bottom-8 right-8">
+            <button
+              onClick={() => setShowModal(true)}  // Mostrar el modal al hacer clic
+              className="relative w-14 h-14 bg-blue-600 text-white rounded-full p-4 hover:bg-blue-700 transition duration-200 group"
+            >
+              <FaPlus className="absolute left-5 bottom-5" />
+              <span className="absolute bottom-full mb-2 w-20 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs rounded py-1 px-2 opacity-0 transition-opacity duration-200 pointer-events-none group-hover:opacity-100">
+                Crear una clase
+              </span>
+            </button>
+          </div>
 
-      {/* Renderizar el modal de creación de clase */}
-      <CreateClassModal
-        isOpen={showModal}  // Controla si el modal está visible
-        onClose={() => setShowModal(false)}  // Función para cerrar el modal
-      />
+          {/* Renderizar el modal de creación de clase */}
+          <CreateClassModal isOpen={showModal} onClose={() => setShowModal(false)} />
+        </>
+      ) :
+        null
+      }
     </>
   );
 }
