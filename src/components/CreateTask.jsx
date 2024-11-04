f82683285edddd9bdc887b9c7deacdc5035b1888
sr-roboto <marcoslopez123.lu@gmail.com>
2024-11-04 14:08:02 -0300

    Refactor CreateTaskModal component and remove unnecessary axios headers

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -4,129 +4,136 @@ import { useTask } from '../context/TasksContext';
 import { useRef, useEffect } from 'react';
 
 function CreateTaskModal({ isOpen, onClose }) {
+  const { classId } = useParams();
 
-    const { classId } = useParams();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
+  // Accedemos a las funciones createTask y getTasks desde el contexto
+  const { createTask, getTasks } = useTask();
 
-    // Accedemos a las funciones createTask y getTasks desde el contexto
-    const { createTask, getTasks } = useTask();
+  const modalRef = useRef(null);
 
-    const modalRef = useRef(null);
+  const handleOutsideClick = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      onClose();
+    }
+  };
 
-    const handleOutsideClick = (event) => {
-        if (modalRef.current && !modalRef.current.contains(event.target)) {
-            onClose();
-        }
+  useEffect(() => {
+    // Agrega el evento de clic en el documento
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    // Limpia el evento cuando el componente se desmonte
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
     };
+  }, []);
 
-    useEffect(() => {
-        // Agrega el evento de clic en el documento
-        document.addEventListener('mousedown', handleOutsideClick);
+  // Función para manejar el envío del formulario
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data.file[0]);
+    try {
+      const formData = new FormData();
+      formData.append('title', data.name);
+      formData.append('description', data.description);
+      formData.append('due_date', data.dueDate);
+      formData.append('file', data.file[0]);
+      await createTask(formData, classId);
+      // Recargar las tareas desde el backend después de crear una nueva tarea
+      await getTasks(classId);
 
-        // Limpia el evento cuando el componente se desmonte
-        return () => {
-            document.removeEventListener('mousedown', handleOutsideClick);
-        };
-    }, []);
+      // Cerrar el modal
+      onClose();
+    } catch (error) {
+      console.error('Error al crear la tarea:', error);
+    }
+  });
 
-    // Función para manejar el envío del formulario
-    const onSubmit = handleSubmit(async (data) => {
-        console.log(data.file[0])
-        try {
-            // Llamada a la función createTask para hacer el POST al backend
-            console.log('create task', data);
-            await createTask(formData, classId);
-            // Recargar las tareas desde el backend después de crear una nueva tarea
-            await getTasks();
+  // Si el modal no está abierto, no se renderiza nada
+  if (!isOpen) return null;
 
-            // Cerrar el modal
-            onClose();
-        } catch (error) {
-            console.error("Error al crear la tarea:", error);
-        }
-    });
-
-    // Si el modal no está abierto, no se renderiza nada
-    if (!isOpen) return null;
-
-    return (
-        <>
-            {/* Fondo oscuro */}
-            <div
-                className="fixed inset-0 bg-black bg-opacity-50 z-40 flex items-center justify-center"
-                onClick={onClose} // Cierra el modal cuando haces clic en el fondo oscuro
-            >
-                {/* Contenedor del modal */}
-                <div ref={modalRef} onClick={(e) => e.stopPropagation()} className="bg-white p-8 rounded-lg shadow-[0px_7px_8px_-4px_rgba(0,0,0,0.53)] max-w-md w-full relative">
-                    <h2 className="text-xl mb-4">Crear una nueva tarea</h2>
-                    <form
-                        onSubmit={onSubmit}
-
-                    >
-                        {/* Campo para el nombre de la tarea */}
-                        <label className="block mb-2">Nombre de la tarea:</label>
-                        <input
-                            type="text"
-                            className="w-full p-2 border border-gray-300 rounded mb-4"
-                            placeholder="Nombre de la tarea"
-                            {...register('name', { required: true })}
-                        />
-                        {errors.name && (
-                            <span className="text-red-500">Este campo es requerido</span>
-                        )}
-                        {/* Campo para la descripción de la tarea */}
-                        <label className="block mb-2">Descripción de la tarea:</label>
-                        <textarea
-                            className="w-full p-2 border border-gray-300 rounded mb-4"
-                            placeholder="Descripción de la tarea"
-                            {...register('description', { required: true })}
-                        ></textarea>
-                        {errors.description && (
-                            <span className="text-red-500">Este campo es requerido</span>
-                        )}
-                        {/* Campo para la fecha de entrega */}
-                        <label className="block mb-2">Fecha de entrega:</label>
-                        <input
-                            type="date"
-                            className='w-full p-2 border border-gray-300 rounded mb-4'
-                            {...register('dueDate', { required: true })}
-                        />
-                        {errors.dueDate && (
-                            <span className="text-red-500">Este campo es requerido</span>
-                        )}
-                        {/* Campo para cargar archivo */}
-                        <label className="block mb-2">Cargar archivo:</label>
-                        <input
-                            type="file"
-                            name='file'
-                            className='w-full p-2 border border-gray-300 rounded mb-4'
-                            {...register('file', { required: true })}
-                        />
-                        {errors.file && (
-                            <span className="text-red-500">Este campo es requerido</span>
-                        )}
-                        {/* Botones para cancelar o enviar */}
-                        <div className="flex justify-end">
-                            <button
-                                type="button"
-                                onClick={onClose}
-                                className="bg-gray-500 text-white px-4 py-2 rounded mr-2 hover:bg-gray-700 transition duration-300"
-                            >
-                                Cancelar
-                            </button>
-                            <button
-                                type="submit"
-                                className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-800 transition duration-300"
-                            >
-                                Crear
-                            </button>
-                        </div>
-                    </form>
-                </div>
+  return (
+    <>
+      {/* Fondo oscuro */}
+      <div
+        className="fixed inset-0 bg-black bg-opacity-50 z-40 flex items-center justify-center"
+        onClick={onClose} // Cierra el modal cuando haces clic en el fondo oscuro
+      >
+        {/* Contenedor del modal */}
+        <div
+          ref={modalRef}
+          onClick={(e) => e.stopPropagation()}
+          className="bg-white p-8 rounded-lg shadow-[0px_7px_8px_-4px_rgba(0,0,0,0.53)] max-w-md w-full relative"
+        >
+          <h2 className="text-xl mb-4">Crear una nueva tarea</h2>
+          <form onSubmit={onSubmit}>
+            {/* Campo para el nombre de la tarea */}
+            <label className="block mb-2">Nombre de la tarea:</label>
+            <input
+              type="text"
+              className="w-full p-2 border border-gray-300 rounded mb-4"
+              placeholder="Nombre de la tarea"
+              {...register('name', { required: true })}
+            />
+            {errors.name && (
+              <span className="text-red-500">Este campo es requerido</span>
+            )}
+            {/* Campo para la descripción de la tarea */}
+            <label className="block mb-2">Descripción de la tarea:</label>
+            <textarea
+              className="w-full p-2 border border-gray-300 rounded mb-4"
+              placeholder="Descripción de la tarea"
+              {...register('description', { required: true })}
+            ></textarea>
+            {errors.description && (
+              <span className="text-red-500">Este campo es requerido</span>
+            )}
+            {/* Campo para la fecha de entrega */}
+            <label className="block mb-2">Fecha de entrega:</label>
+            <input
+              type="date"
+              className="w-full p-2 border border-gray-300 rounded mb-4"
+              {...register('dueDate', { required: true })}
+            />
+            {errors.dueDate && (
+              <span className="text-red-500">Este campo es requerido</span>
+            )}
+            {/* Campo para cargar archivo */}
+            <label className="block mb-2">Cargar archivo:</label>
+            <input
+              type="file"
+              name="file"
+              className="w-full p-2 border border-gray-300 rounded mb-4"
+              {...register('file', { required: true })}
+            />
+            {errors.file && (
+              <span className="text-red-500">Este campo es requerido</span>
+            )}
+            {/* Botones para cancelar o enviar */}
+            <div className="flex justify-end">
+              <button
+                type="button"
+                onClick={onClose}
+                className="bg-gray-500 text-white px-4 py-2 rounded mr-2 hover:bg-gray-700 transition duration-300"
+              >
+                Cancelar
+              </button>
+              <button
+                type="submit"
+                className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-800 transition duration-300"
+              >
+                Crear
+              </button>
             </div>
-        </>
-    );
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CreateTaskModal;
