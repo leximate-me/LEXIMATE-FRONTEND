b36f7ee653a1eab15d08682494d75fae335aee79
sr-roboto <marcoslopez123.lu@gmail.com>
2024-11-16 11:35:34 -0300

    Actualizar 404.html para redirigir a index.html y cambiar BrowserRouter a HashRouter en App.jsx para mejorar la compatibilidad con rutas.

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         <PostProvider>
           <TaskProvider>
             <ToolProvider>
-              <BrowserRouter basename="/">
+              <HashRouter>
                 <main className="container min-w-full min-h-[100vh] dark:bg-[#1a1a1a]">
                   <NavBar />
                   <div className="pt-[65px]">
@@ -55,7 +55,7 @@ function App() {
                     </Routes>
                   </div>
                 </main>
-              </BrowserRouter>
+              </HashRouter>
             </ToolProvider>
           </TaskProvider>
         </PostProvider>
