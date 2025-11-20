6035ab8ec87b65091fdeeb163a7938e3a0aeb175
sr-roboto <marcoslopez123.lu@gmail.com>
2025-11-20 03:58:40 -0300

    chore: Update Vite dev server host to `0.0.0.0` and API proxy target to `http://backend:8080`, and remove unused `TARGET_API_URL`.

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,14 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
   server: {
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 5173,
     watch: {
       usePolling: true,
     },
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://backend:8080',
         changeOrigin: true,
       }
     }
